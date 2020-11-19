129ddb15b0ee18b26640ecea14f8a84b5527bca4
NikhilBansal1999 <nikhilba@iitk.ac.in>
2020-11-19 20:49:42 +0530

    Added code to plot heatmap

--- literacy_data.py
+++ literacy_data.py
@@ -21,20 +21,49 @@ for district in year_data:
 literacy_data = pd.DataFrame.from_dict(literacy_data)
 
 map_df = gpd.read_file('Census_2011/2011_Dist.shp')
-map_df = map_df[['DISTRICT', 'ST_NM', 'geometry']]
+map_df = map_df[['DISTRICT', 'geometry']]
+
 
 districts = list(map_df["DISTRICT"])
-dist_in_map = []
-states = list(map_df["ST_NM"])
-for i in districts:
-	dist_in_map.append(i.lower())
 
-total = 0
-in_map = 0
-for dist in literacy_data['District']:
-	total = total + 1
-	if dist.lower() in dist_in_map:
-		in_map = in_map + 1
+global_dict_file = open("global_map.pkl","rb")
+global_map = pickle.load(global_dict_file,encoding='utf-8')
+global_dict_file.close()
 
-for i in range(len(dist_in_map)):
-	print(i,dist_in_map[i], states[i])
+def get_district_code(dist_name):
+	for dist in global_map:
+		if "_" in dist:
+			if dist_name == dist.split("_")[0]:
+				return global_map[dist]
+		else:
+			if dist_name == dist:
+				return global_map[dist]
+	return -1
+
+literacy_dict = dict()
+literacy_dict['District'] = districts
+literacy_dict['Literacy'] = [0]*len(districts)
+count = 0
+for i in range(len(literacy_data['District'])):
+	code = get_district_code(literacy_data['District'][i].lower())
+	if code == -1:
+		count = count + 1
+		print(literacy_data['District'][i])
+	else:
+		if code < len(districts):
+			literacy_dict['Literacy'][code] = literacy_data['literacy_rate'][i]
+
+if count > 0:
+	print(count,"not found in global map")
+dataframe = pd.DataFrame.from_dict(literacy_dict)
+dataframe.to_excel("output.xlsx")
+
+df = pd.read_excel('output.xlsx')
+merged = map_df.set_index('DISTRICT').join(df.set_index('District'))
+merged = merged[['Literacy', 'geometry']]
+
+fig, ax = plt.subplots(1, figsize=(10, 6))
+ax.axis('off')
+ax.set_title('District Wise Literacy Rate in 2013-14', fontdict={'fontsize': '25', 'fontweight' : '3'})
+merged.plot(column='Literacy', cmap='Oranges', linewidth=0.8, ax=ax, edgecolor='0.8', legend=False, categorical=False)
+fig.savefig("District_wise_literacy.png", dpi=100)
